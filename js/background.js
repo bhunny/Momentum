56701de48f9b2a9ff7d4713e63b967ceb089f9c0
lim7217 <lim7217@naver.com>
2021-10-04 05:41:44 +0900

    Modify CSS

--- js/background.js
+++ js/background.js
@@ -5,7 +5,10 @@ for (var i = 0; i<10; i++){
 }
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
+//const bgImage = document.createElement("img");
+//bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+//document.body.appendChild(bgImage);
+
+const body = document.querySelector("body");
+body.style = `background-image: url(img/${chosenImage})`;
